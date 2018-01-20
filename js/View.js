9008795b34a00defaf4ed69144a6bff086bcef48
戴江涛 <captaininphw@gmail.com>
2018-01-20 21:39:08 +0800

    View.js

--- js/View.js
+++ js/View.js
@@ -4,10 +4,15 @@
             homepage: $('#site-homepage'),
             option: $('#site-dropdown-button')[0],
             content: $('#site-search-content')[0],
-            OpimizeSearchContentReg: new RegExp(/[a-zA-Z0-9\u4e00-\u9fa5]+/,'g')
+            OpimizeSearchContentReg: new RegExp(/[a-zA-Z0-9\u4e00-\u9fa5]+/,'g'),
+            init: function () {
+                userView.init();
+            }
         };
         for (let key in userView) {
-            view[key] = userView[key];
+            if (key !== 'init'){
+                view[key] = userView[key];
+            }
         }
         return view;
     }
