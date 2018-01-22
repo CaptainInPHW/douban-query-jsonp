fc44fe36c80f10bc4e05b98fe9851ab2600bd168
戴江涛 <captaininphw@gmail.com>
2018-01-22 18:34:11 +0800

    Model.js

--- js/Model.js
+++ js/Model.js
@@ -3,18 +3,25 @@
         let model = {
             option: undefined,
             searchContent: undefined,
+            OpimizeSearchContentReg: new RegExp(/[a-zA-Z0-9\u4e00-\u9fa5]+/,'g'),
             getOption: function (view) {
-                this.option = (function (option) {
+                let option = (function (option) {
                     return option === '类别' ?  alert('请选择查询类别！') : option;
                 })(view.option.innerText.trim());
+                switch (option) {
+                    case '图书': this.option = 'books'; break;
+                    case '电影': this.option = 'movies'; break;
+                    case '音乐': this.option = 'musics'; break;
+                    default: this.option = undefined;
+                }
                 return this.option;
             },
             getSearchContent: function (view) {
                 this.searchContent = (function (searchContent) {
                     return searchContent === null ? alert('请选择查询内容！') : searchContent.join('');
-                })(view.content.value.match(view.OpimizeSearchContentReg));
+                })(view.content.value.match(model.OpimizeSearchContentReg));
                 return this.searchContent;
-            },
+            }
         };
         for (let key in userModel) {
             model[key] = userModel[key];
